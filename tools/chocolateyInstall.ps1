521ac182b60165c094a11e9a27255623cbef31ad
Robert Coleman <github@robert.net.nz>
2014-02-23 08:15:41 +1300

    add gitattributes file

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,19 +1,19 @@
 ﻿try {
-	$packageName = 'awscli'
-	$installerType = 'msi'
-	$url = 'https://s3.amazonaws.com/aws-cli/AWSCLI32.msi'
-	$url64 = 'https://s3.amazonaws.com/aws-cli/AWSCLI64.msi'
-	$silentArgs = '/qn'
-	$validExitCodes = @(0)
+  $packageName = 'awscli'
+  $installerType = 'msi'
+  $url = 'https://s3.amazonaws.com/aws-cli/AWSCLI32.msi'
+  $url64 = 'https://s3.amazonaws.com/aws-cli/AWSCLI64.msi'
+  $silentArgs = '/qn'
+  $validExitCodes = @(0)
 
-	Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
-	$programFiles = $env:programfiles
-	$awscliPath = Join-Path $programFiles 'Amazon\AWSCLI'
+  Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
+  $programFiles = $env:programfiles
+  $awscliPath = Join-Path $programFiles 'Amazon\AWSCLI'
 
-	Install-ChocolateyPath $awscliPath 'machine'
-	$env:Path += ";$awscliPath"
-	Write-ChocolateySuccess "$packageName"
+  Install-ChocolateyPath $awscliPath 'machine'
+  $env:Path += ";$awscliPath"
+  Write-ChocolateySuccess "$packageName"
 } catch {
-	Write-ChocolateyFailure "$packageName" "$($_.Exception.Message)"
-	throw
+  Write-ChocolateyFailure "$packageName" "$($_.Exception.Message)"
+  throw
 }
